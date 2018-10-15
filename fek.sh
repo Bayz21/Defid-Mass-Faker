bf038c9ca4c301e86e25c6bfc0cddce1d30e5b3b
Bayz21 <39861892+Bayz21@users.noreply.github.com>
2018-10-15 10:07:06 -0400

    Add files via upload

---
+++ fek.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Coded By Bayz21
+function mass() {
+printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 1.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 2.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 3.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 4.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 5.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 6.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 7.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 8.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 9.txt & printf "http://jctneverdie.$RANDOM.$RANDOM.$RANDOM.$RANDOM.$RANDOM.go.gob.edu.ac.gov.joynmercy.com\n" >> 10.txt
+}
+cat << "Bayz21"
+
+██████╗  █████╗ ██╗   ██╗███████╗██████╗  ██╗
+██╔══██╗██╔══██╗╚██╗ ██╔╝╚══███╔╝╚════██╗███║
+██████╔╝███████║ ╚████╔╝   ███╔╝  █████╔╝╚██║
+██╔══██╗██╔══██║  ╚██╔╝   ███╔╝  ██╔═══╝  ██║
+██████╔╝██║  ██║   ██║   ███████╗███████╗ ██║
+╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝ ╚═╝
+---------------------------------------------
+-------------- Defid MASS Faker -------------
+---------------------------------------------
+ 
+Bayz21
+echo -n "Masukan Jumlah Fake : ";
+read jumlah
+echo "Running :3";
+count=1
+while [ $count -le $jumlah ]
+do
+	mass $fek;
+(( count++ ))
+done
+while true
+do
+python submit.py 1.txt KimochiSenpai "Jembud Cyber Team" & python submit.py 2.txt AntiFake "Jembud Cyber Team" & python submit.py 3.txt Ped0pile "Jembud Cyber Team" & python submit.py 4.txt PecintaL0L1 "Jembud Cyber Team" & python submit.py 5.txt c0Lm3k "Jembud Cyber Team" & python submit.py 6.txt Aswlo123 "Jembud Cyber Team" & python submit.py 7.txt KimochiSenpai "Jembud Cyber Team" & python submit.py 8.txt NgeueKuy "Jembud Cyber Team" & python submit.py 9.txt JCT4ever "Jembud Cyber Team" & python submit.py 10.txt WetPussy "Jembud Cyber Team"
+done
